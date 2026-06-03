d1150f78e95da725481b793585f574b28fdfb3da
user <necheporuker@ingrad.com>
2026-06-03 21:15:48 +0300

    fix api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -129,7 +129,7 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
-    yaKey: '',
+    yaApi: '',
     yaGeocodeApi: '',
     deepseekApiKey: '',
     public: {
--- server/api/suggest.js
+++ server/api/suggest.js
@@ -1,16 +1,14 @@
+const config = useRuntimeConfig();
+
+const initialParams = {
+  apikey: config.yaApi || process.env.YA_API,
+  lang: 'ru_RU',
+  type: 'geo',
+  results: 10,
+  types: 'locality',
+};
+
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig();
-
-  const initialParams = {
-    apikey: config.yaKey || process.env.YA_API,
-    lang: 'ru_RU',
-    type: 'geo',
-    results: 10,
-    types: 'locality',
-  };
-
-  console.log(useRuntimeConfig());
-
   const params = getQuery(event);
 
   if (!params) {
